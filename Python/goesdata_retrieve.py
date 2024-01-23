5ae9f5e99d9c6ea857becc5181aee3bb0315cccc
lmallett <lucien.mallett@gmail.com>
2024-01-22 16:33:01 -0800

    Updates
    - Reworked GOES graphs based on sunpy tutorial
    - Wrote function to produce non-normalized response function graphs
    - integrated TeX support for prettiness

--- Python/goesdata_retrieve.py
+++ Python/goesdata_retrieve.py
@@ -1,16 +1,17 @@
 # https://docs.sunpy.org/en/latest/generated/gallery/time_series/goes_hek_m25.html
 
 import matplotlib.pyplot as plt
+plt.rcParams['text.usetex'] = True
 
 from sunpy.net import Fido
 from sunpy.net import attrs as a
 from sunpy.time import parse_time
 from sunpy.timeseries import TimeSeries
 
-# tstart = "2011-08-09 07:00"
-# tend = "2011-08-09 10:00"
-tstart = "2014-09-10 17:00"
-tend = "2014-09-10 23:00"
+tstart = "2011-08-09 07:00"
+tend = "2011-08-09 10:00"
+# tstart = "2014-09-10 17:00"
+# tend = "2014-09-10 23:00"
 tr = a.Time(tstart, tend)
 
 results = Fido.search(tr, a.Instrument("XRS") & a.goes.SatelliteNumber(15) & a.Resolution("flx1s") | a.hek.FL & (a.hek.FL.GOESCls > "X1.0") & (a.hek.FRM.Name == 'SWPC'))
@@ -32,5 +33,7 @@ ax.axvspan(
 ax.legend(loc=2)
 ax.set_yscale('log')
 ax.set_xlim(tr.start.to_datetime(), tr.end.to_datetime())
+ax.set_ylabel(r'SXR Flux $(\mathrm{W} \; \mathrm{m}^{-2})$')
+ax.set_xlabel("Time")
 
 plt.show()
--- Python/lociplots.py
+++ Python/lociplots.py
@@ -7,6 +7,8 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+plt.rcParams['text.usetex'] = True
+
 import idl_colorbars
 import astropy.units as u
 import sunpy.visualization.colormaps.color_tables as aiacolormaps
@@ -123,12 +125,10 @@ def looptrace(wavelength = allwaves):
 
     plt.close()
 
-
 ######################################################################
 ######################################################################
 ######################################################################
 ######################################################################
-
 
 # select coronal loop pixels
 # pick the brightest pixels in the loop that converge
@@ -136,7 +136,6 @@ def looptrace(wavelength = allwaves):
 # find loci plots of those
 # pick non coronal loop pixels in the ROI
 # find loci plots of those    
-
 
     # logt=indgen(20)*0.1+5.6
     # r=aia_get_response(/dn,/temperature,/full,/evenorm,timedepend_date='30-Jul-2021 18:00:00')
@@ -234,14 +233,13 @@ def lociplots(x, y):
         plt.savefig(fname = savedir + "lociplots_140-250/" + file[:-4] + ".png")
         # fig.show()
 
-
 ######################################################################
 ######################################################################
 ######################################################################
 ######################################################################
 
 def norm_temp_responses():
-    """Plot the temperature response functions for AIA """
+    """Plot the temperature response functions for AIA, normalized, to show where each channel is most sensitive."""
 
     # load in response functions as 101x1 arrays
     respinfo = sp.io.readsav("aia_response_2011.sav", python_dict = False, verbose = True)
@@ -277,7 +275,7 @@ def norm_temp_responses():
 
     axs.set_xlim(4.5, 7.5)
     axs.set_ylim(0, 1.1)
-    axs.set_xlabel("$log(T)$")
+    axs.set_xlabel(r"$\log_{10}(T/\mathrm{K})$")
     axs.set_ylabel("Normalized AIA Temp. Response")
 
     for j in range(6):
@@ -286,6 +284,50 @@ def norm_temp_responses():
         
         # plot responses
         axs.plot(logte, respj, color = linecolors[j], label = allwaves[j])
+    
+    axs.legend()
+    plt.show()
+
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+
+def temp_responses():
+    """Plot the temperature response functions for AIA to show where each channel is most sensitive."""
+
+    # load in response functions as 101x1 arrays
+    respinfo = sp.io.readsav("aia_response_2011.sav", python_dict = False, verbose = True)
+    r94 = respinfo['r94']
+    r131 = respinfo['r131']
+    r171 = respinfo['r171']
+    r193 = respinfo['r193']
+    r211 = respinfo['r211']
+    r335 = respinfo['r335']
+    logte = ((respinfo['r'])['logte'])[0]
+
+    resp = [r94, r131, r171, r193, r211, r335]
+    linecolors = ['r','g','b', 'c', 'm', 'y']
+
+    fig, axs = plt.subplots()
+
+    axs.minorticks_on()
+    axs.tick_params(axis='both', which = 'both',
+                        direction = 'in',
+                        bottom = True, top= True, left= True, right = True)
+    axs.tick_params(which = 'major', length = 10)
+    axs.tick_params(which = 'minor', length = 5)
+
+    axs.set_xlim(4.5, 7.5)
+    axs.set_ylim(10**(-28), 10**(-23))
+    axs.set_xlabel(r"$\log_{10}(T/\mathrm{K})$")
+    axs.set_ylabel(r"AIA Temp. Response $(\mathrm{DN}\;\mathrm{cm}^5 \; \mathrm{px}^{-1})$")
+
+    for j in range(6):
+        
+        # plot responses
+        respj = resp[j]
+        axs.semilogy(logte, respj, color = linecolors[j], label = allwaves[j])
     
     axs.legend()
     plt.show()
