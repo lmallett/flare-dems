9cf8d83508292a16e47ac1e8540beb5c02eed80f
lmallett <lucien.mallett@gmail.com>
2025-06-05 01:28:50 -0700

    Various cleanup and commenting

--- python code/make_imgs.py
+++ python code/make_imgs.py
@@ -1,25 +1,36 @@
 # (c) Lucien Mallett 2024
 # Produces images from .npz files containing saved variables.
+# The .npz files are assumed to contain the following arrays:
+#       datacube:   a 6 x n x m cube containing the DN for each pixel.
+#       satmap:     a 6 x n x m cube containing a binary 1/0 for whether that pixel is saturated in one of the given 6 channels.
+#       emcube:     a z x n x m cube. at each pixel (n,m), there is a z-length array that describes the emission measure function, where z is the number of temperature bins (logT).
+#       statuscube: an n x m array. at each pixel, there is a value 0, 1, 2, 3, 10, 11 representing the status of the emission cube program. 
+            # 1: The objective function is unbounded.
+            # 2: No solution satisfies the given constraints.
+            # 3: The routine did not converge.
+            # 10: SIMPLEX very occasionally returns negative coeffs even though positivity is a constraint. Now these are set to zero and the corresponding status code is 10.
+            # 11: NOCOUNTS = WHERE(total(image,3) LT 10*eps). Set the status value of nocount pixels to 11.0
 
 import matplotlib
-matplotlib.use("Agg")
-
 import matplotlib.colors as colors
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+plt.rc('text',usetex = True)
+font = {'family':'serif','size':16}
+plt.rc('font',**font)
+
+matplotlib.use("Agg")
+matplotlib.rcParams['lines.linestyle'] = '-'
+matplotlib.rcParams['lines.linewidth'] = 1
+
+import sunpy.visualization.colormaps.color_tables as aiacolormaps
 
 import numpy as np
 import os
-import matplotlib.pyplot as plt
-import sunpy.visualization.colormaps.color_tables as aiacolormaps
+
 import astropy.units as u
+from aiapy import calibrate
 
-
-plt.rc('text',usetex = True)
-font = {'family':'serif','size':16}#, 'serif': ['computer modern roman']}
-plt.rc('font',**font)
-
-mpl.rcParams['lines.linestyle'] = '-'
-mpl.rcParams['lines.linewidth'] = 1
 ##############################################
 ##############################################
 # Path to folder containing .npz files, which contain image data at different timesteps
@@ -46,6 +57,7 @@ path = "D://emcubes_140910//emcubes_np//"
 savedir = "C://Users//Lucien//Documents//School//Research//2023 - DEM Inversion//movies//2014//"
 xregion = [350,650]
 yregion = [380,530]
+
 ##############################################
 
 dir_emcubes = savedir + "emcubes//"
@@ -89,6 +101,8 @@ sat_color = colors.ListedColormap("#e41a1c")
 
 allwaves  = [94,131,171,193,211,335]
 
+px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+
 ##############################################
 ##############################################
 # Functions
@@ -96,7 +110,7 @@ allwaves  = [94,131,171,193,211,335]
 ##############################################
 
 def imgs_emcubes(contour_sat = False, color_sat = False, status = False):
-    """Produces frames for a movie of the event, where the data plotted is the sum of the emission cubes over logT.
+    """Produces and saves an image for all timestamps in the event. The plotted data is the sum of the emission measure over logT for each pixel.
     Args:
         None.
     Returns:
@@ -112,7 +126,6 @@ def imgs_emcubes(contour_sat = False, color_sat = False, status = False):
         vars = np.load(filename)
         emcube = (np.sum( (vars["emcube"])[:,ystart:yend,xstart:xend], axis = 0)**0.25)
 
-        px = 1/plt.rcParams['figure.dpi']  # pixel in inches
         fig, ax = plt.subplots(figsize=(emcube.shape[1]*2*px, emcube.shape[0]*2*px),
                 frameon = False)
         ax.set_axis_off()
@@ -215,7 +228,6 @@ def imgs_statuscubes(contour_sat = False):
         print(filename)
 
         vars = np.load(filename)
-
         statuscube = (vars["statuscube"])[ystart:yend, xstart:xend]
 
         # 1: The objective function is unbounded.
@@ -224,7 +236,6 @@ def imgs_statuscubes(contour_sat = False):
         statuscube[statuscube == 10] = 4    # SIMPLEX very occasionally returns negative coeffs even though positivity is a constraint. Now these are set to zero and the corresponding status code is 10.
         statuscube[statuscube == 11] = 5    # NOCOUNTS = WHERE(total(image,3) LT 10*eps). Set the status value of nocount pixels to 11.0
 
-        px = 1/plt.rcParams['figure.dpi']  # pixel in inches
         fig, ax = plt.subplots(figsize=(statuscube.shape[1]*px, statuscube.shape[0]*px), frameon = False)
         ax.set_axis_off()
         
@@ -280,7 +291,7 @@ def imgs_datacubes(wavelength = allwaves):
 
     colors = [] # list of colormaps
     for e in indices:
-        colors.append(aiacolormaps.aia_color_table(allwaves[e] * u.Angstrom))
+        colors.append(aiacolormaps.aia_color_table(u.Quantity(allwaves[e], 'Angstrom')))
 
     for i in range(len(filelist)):
 
@@ -300,7 +311,6 @@ def imgs_datacubes(wavelength = allwaves):
 
     plt.close()
 
-
 def imgs_allchannels():
     """Produces frames for a movie of the event in all wavelengths.
     Args:
@@ -312,7 +322,63 @@ def imgs_allchannels():
 
     colors = [] # list of colormaps
     for e in range(6):
-        colors.append(aiacolormaps.aia_color_table(allwaves[e] * u.Angstrom))
+        colors.append(aiacolormaps.aia_color_table(u.Quantity(allwaves[e], 'Angstrom')))
+
+    # Calculate the correct figure size based on data dimensions
+    data_width = xend - xstart
+    data_height = yend - ystart
+
+    nrows = 2
+    ncols = 3
+    height = [data_height]*nrows
+    width = [data_width]*ncols
+
+    print(data_width, data_height)
+    print(data_width*ncols, data_height*nrows)
+
+    fig, axs = plt.subplots(2, 3,
+            figsize=(sum(width)*2*px, sum(height)*2*px),
+            gridspec_kw={'height_ratios':height})
+
+    titles = ['$94 \mathrm{\AA}$', 
+            '$131 \mathrm{\AA}$', 
+            '$171 \mathrm{\AA}$', 
+            '$193 \mathrm{\AA}$', 
+            '$211 \mathrm{\AA}$',
+            '$335 \mathrm{\AA}$']
+
+    axs = np.reshape(axs, (6))
+
+    for i in range(len(filelist)):
+        
+        file = filelist[i]
+        data = np.load(path + file)
+        data = np.clip((data["datacube"])[:, ystart:yend, xstart:xend], a_min=0, a_max=None)**0.25
+
+        print(i)
+
+        for j in range(6):
+
+            axs[j].imshow(data[j],
+                        interpolation = 'none',
+                        origin = 'lower',
+                        cmap = colors[j])
+            axs[j].axis('off')
+            axs[j].text(0.02, 0.88,  # x, y
+                        titles[j],  # string
+                        transform = axs[j].transAxes,   # places on each axis individually
+                        c = 'white',
+                        size = 'x-large',
+                        )
+
+        plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
+        fig.savefig(savedir + "data_allchannels//" + file[:-4] + ".png")
+
+def imgs_rel_err():
+
+    colors = [] # list of colormaps
+    for e in range(6):
+        colors.append(aiacolormaps.aia_color_table(u.Quantity(allwaves[e], 'Angstrom')))
 
     # Calculate the correct figure size based on data dimensions
     data_width = xend - xstart
@@ -322,8 +388,6 @@ def imgs_allchannels():
 
     print(data_width, data_height)
     print(data_width*3, data_height*2)
-
-    px = 1/plt.rcParams['figure.dpi']  # pixel in inches
 
     fig, axs = plt.subplots(2, 3,
             figsize=(sum(width)*px, sum(height)*px),
@@ -339,52 +403,42 @@ def imgs_allchannels():
     axs = np.reshape(axs, (6))
 
     for i in range(len(filelist)):
+        
         file = filelist[i]
-        data = np.load(path + file)
-        data = np.clip((data["datacube"])[:, ystart:yend, xstart:xend], a_min=0, a_max=None)**0.25
-
-        print(i)
+        vars = np.load(path + file)
+        data = (vars["datacube"])[:, ystart:yend, xstart:xend]
+        data = data.clip(min = 0)
+        data_quant = u.Quantity(data, 'ct/pix')
+        print(file)
         for j in range(6):
-            axs[j].imshow(data[j],
+
+            err = calibrate.estimate_error(data_quant[j,:,:], u.Quantity(allwaves[j], 'Angstrom'))
+
+            rel_err = np.divide(err, data_quant[j,:,:], out=np.zeros_like(err), where=data_quant[j,:,:]!=0)*100
+
+            print("min", np.min(rel_err))
+            print("max", np.max(rel_err))
+
+            axs[j].imshow(rel_err,
                         interpolation = 'none',
                         origin = 'lower',
-                        cmap = colors[j])
+                        vmin = 0,
+                        vmax = 100,
+                        cmap = 'Greys')
             axs[j].axis('off')
-            axs[j].text(0.02, 0.9,  # x, y
+
+            axs[j].text(0.02, 0.86,  # x, y
                         titles[j],  # string
                         transform = axs[j].transAxes,   # places on each axis individually
-                        c = 'white',
-                        size = 'xx-large',
+                        c = 'red',
+                        # size = 'large',
                         )
+            # axs[j].set_xticks([])
+            # axs[j].set_yticks([])
+
+        # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+        # fig.colorbar(im, cax=cbar_ax)
 
         plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
-        fig.savefig(savedir + "data_allchannels//" + file[:-4] + ".png")
+        fig.savefig(savedir + "rel_err//" + file[:-4] + ".png")
 
-
-def imgs_rel_err():
-
-    for i in range(len(filelist)):
-
-        file = filelist[i]
-        filename = path + file
-        print(filename)
-
-        vars = np.load(filename)
-        emcube = (np.sum( (vars["emcube"])[:,ystart:yend,xstart:xend], axis = 0)**0.25)
-
-        px = 1/plt.rcParams['figure.dpi']  # pixel in inches
-        fig, ax = plt.subplots(figsize=(emcube.shape[1]*2*px, emcube.shape[0]*2*px),
-                frameon = False)
-        ax.set_axis_off()
-
-        ax.imshow(emcube, cmap = 'gray', interpolation = 'none', origin = 'lower',)
-
-        plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
-        fig.savefig(dir_emcubes + file[:-4] + " " + str(i) + ".png", 
-                    pad_inches = 0, 
-                    bbox_inches= 'tight',
-                    )        
-        
-        plt.clf()
-
-    return
